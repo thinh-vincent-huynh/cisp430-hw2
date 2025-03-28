835ace0bc7501eb2a587ce66f702c330a838dce8
Vincent Huynh <thinh.vincent.huynh@gmail.com>
2025-03-28 10:01:42 -0700

    final

--- huynhv-hw2.cpp
+++ huynhv-hw2.cpp
@@ -1,8 +1,11 @@
+#include <iostream>
 using namespace std;
 
-SIZE = 10;
-ARRAY[SIZE] = [7, 4, 8, 11, 6, 5, 10, 3, 9, 2];
+// array to be populated
+const int SIZE = 10;
+int ARRAY[SIZE] = {7, 3, 8, 11, 6, 10, 10, 3, 9, 11};
 
+// node struct is enough for linked list implementation
 struct node{
     int value;
     node* prev;
@@ -14,51 +17,79 @@ struct node{
     }
 };
 
+//split a linked list into 2 evenly sub linked lists
 void split(node* head,node*&a,node*&b){
-    a = b = head;
-    count = 1;
+    a = head;   // start at head; a stays at head
+    b = head;   // start at head and end at middle
+    int count = 1;      // head counts as 1 node
     while (b->next){
-        b = b->next;
-        count++;
+        b = b->next;    // move b to the end
+        count++;        // count num of nodes in list
     }
-    for (int i = 0; i < count / 2; i++){
-        b = b->prev;
+    for (int i = 1; i < count / 2; i++){
+        b = b->prev;    // move b to the middle
     }
-    b->prev = b->prev->next = NULL;
-}
+    // disconnect 2 evenly sub linked lists
+    if (b->prev){   // if NOT only 1 node in list
+        b->prev->next = nullptr;
+        b->prev = nullptr;
+    }
+} // 17 lines
 
 node* merge(node* a, node* b){
-    node* c = a;
-    while (a->next){ a = a->next; }
-    a->next = b;
-    b->prev = a;
-    return c;
-}
+    if (!a){ return b; }    // merge with b itself
+    if (!b){ return a; }    // merge with a itself
+    node* c = nullptr;
+    if (a->value < b->value){   // merge and sort
+        c = a;
+        c->next = merge(a->next, b);
+        if (c->next){ c->next->prev = c; }
+    }else{
+        c = b;
+        c->next = merge(a, b->next);
+        if (c->next){ c->next->prev = c; }
+    }
+    return c;   // the whole list consisted of a and b
+} // 15 lines
 
 node* mergeSort(node* head){
-    if (head==NULL){
-        return NULL;
+    if (head==nullptr){    // split into nothing
+        return nullptr;
     }
-    if (head->next==NULL){
+    if (head->next==nullptr){  // split into itself
         return head;
     }
-    node* a = NULL, b = NULL;
-    split(head, a, b);
-    a = mergeSort(a);
-    b = mergeSort(b);
-    if (a->value < b->value){
-        c = merge(a,b);
-    }else{
-        c = merge(b,a);
-    }
-    return c;
-}
+    // actually start here
+    node *a = nullptr, *b = nullptr;
+    split(head, a, b);  // split into 2 halves
+    a = mergeSort(a);   // continue to split itself
+    b = mergeSort(b);   // continue to split itself
+    return merge(a, b); // sorted partition/whole list
+} // 14 lines
 
 int main(){
-    node* head = new node(ARRAY[0], NULL, NULL);
+    // populate linked list through array
+    // and display the unsorted linked list
+    node* head = new node(ARRAY[0], nullptr, nullptr);
+    cout << "Unsorted:" << endl << head->value;
     node* curr = head;
     for (int i = 1; i < SIZE; i++){
-        curr->next = new node(ARRAY[i], curr, NULL);
+        cout << " -> ";
+        curr->next = new node(ARRAY[i], curr, nullptr);
+        cout << curr->next->value;
         curr = curr->next;
     }
+
+    // sort the linked list using mergeSort()
+    head = mergeSort(head);
+
+    // display the sorted version of the linked list
+    cout << endl << "Sorted:" << endl << head->value;
+    curr = head->next;
+    while (curr){
+        cout << " -> ";
+        cout << curr->value;
+        curr = curr->next;
+    }
+    cout << endl;
 }
